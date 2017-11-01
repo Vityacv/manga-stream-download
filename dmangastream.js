a368b3d4210c8686826715a3971e4fe0b2035581
Vityacv <vityacv@gmail.com>
2017-11-01 20:27:48 +0300

    html elements update

--- dmangastream.js
+++ dmangastream.js
@@ -1,10 +1,17 @@
 #!/bin/node
 var fs = require('fs');
 var os = require('os');
+var url = require('url');
 var request = require('request');
 var cheerio = require('cheerio');
 const execSync = require('child_process').execSync;
 const args = process.argv;
+
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
 
 function getMangaPage(page){
   request('https://images-onepick-opensocial.googleusercontent.com/gadgets/proxy?container=a&url='+encodeURIComponent(page), function (error, response, body) {
@@ -12,9 +19,11 @@ function getMangaPage(page){
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(body);
       var arr = [];
-      next = $('.next').find('a').attr('href');
+      var href = url.parse(page);
+      next = url.parse($('.next').find('a').attr('href'));
+      next = href.protocol+"//"+href.host+next.path;
       var img = $('.page').find('img').attr('src');
-      var name = $('.btn').text().replace(/( )/gm,"").replace(/(\r\n|\n|\r)/gm,"_")+'.png';
+      var name = $('.hidden-sm').text().replace(/( )/gm,"")+$('.btn-primary').text().replace(/( )/gm,"").replace(/(\r\n|\n|\r)/gm,"_")+'.png';
       if(img !== undefined)
       {
         if(img.charAt(0)!=='h')
@@ -25,7 +34,7 @@ function getMangaPage(page){
       }else process.exit(0);
       if(next === undefined)
         process.exit(0);
-      getMangaPage(next);
+      getMangaPage( next);
     } else {
       console.log('Failed to download: '+page);
       process.exit(0);
